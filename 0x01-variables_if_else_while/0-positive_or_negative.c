daee782acc0413ab96e1bafb750e2123a283569c
freittin <nideologies@gmail.com>
2022-07-04 12:24:37 -0700

    betty compliance

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-/*https://github.com/holbertonschool/Betty.git*/
-
 /**
- * main -> assign a random number to the variable n each time it is executed and prints out |
- *based a condition
- *Return : always 0
+ * main ->assign a random number to the variable n each time it is executed
+ * and prints out
+ * based a condition 
+ * Return: Always 0 (Success)
  */
 int main(void)
-
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-	printf("%d is positive\n", n);
-if (n==0)
-	printf("%d is zero\n", n);
+printf("%d is positive\n", n);
+if (n == 0)
+printf("%d is zero\n", n);
 if (n < 0)
-	printf("%d is negative\n", n);
+printf("%d is negative\n", n);
 return (0);
 }
