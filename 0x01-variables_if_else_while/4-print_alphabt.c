270d860d2cf3c026f80fd358b1420c4d2a45cb87
freittin <nideologies@gmail.com>
2022-07-04 19:09:20 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main ->print all the letters except q and e
+ * only use the putchar function (every other function
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	{
+		if (ch != 'q' && ch != 'e'
+
+		{
+			putchar(ch);
+		}
+	}
+	putchar(10); /*new line*/
+	return (0);
+}
