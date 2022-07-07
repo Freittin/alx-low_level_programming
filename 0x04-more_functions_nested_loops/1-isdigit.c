bd335fdfb7c9b0822ff69ff00918d26280538fdc
freittin <nideologies@gmail.com>
2022-07-07 16:20:28 -0700

    pointing digits

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,17 +3,12 @@
 
 /**
  * _isdigit - check if a character is a digit
- * @x : The number to be checked
+ * @c : The number to be checked
  *
  * Return: 1 for a character that will be a digit or 0 for any else
  */
 
 int _isdigit(int c)
-
 {
-	if (x >= 48 && x <= 57)
-	{
-		return (1);
-	}
-	return (0);
+	return (c <= '0' && c >= '9');
 }
