5cc9663d34b5a0ef9849e7832a82aea6456611b2
freittin <nideologies@gmail.com>
2022-07-08 13:13:56 -0700

    fix 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,13 @@ void more_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y < 14; y++)
+		for (y = 0; y <= 14; y++)
 		{
-			if (y >= 9)
+			if (y > 9)
 			{
-				putchar((y / 10) + '0');
+				_putchar((y / 10) + '0');
 		}
-		putchar((y % 10) + '0');
+		_putchar((y % 10) + '0');
 	}
-	putchar(10);
+	_putchar(10);
 }
