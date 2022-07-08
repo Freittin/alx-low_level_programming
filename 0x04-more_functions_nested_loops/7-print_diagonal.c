d2fa9fad5feeb54cb825f5b80f6a50de649169c2
freittin <nideologies@gmail.com>
2022-07-07 18:02:13 -0700

    hot fitx

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,15 +11,15 @@ void print_diagonal(int n)
 	int x, i;
 
 	for (n <= 0)
-		putchar('\n');
+		_putchar('\n');
 	else
 	{
 		for (x = 0; x < n; x++)
 		{
 			for (i = 0; i < x; i++)
-				putchar(' ');
-			putchar(92);
-			putchar('\n');
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
 		}
 	}
 }
