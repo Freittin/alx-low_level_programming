3124cb427f9e116deb0eb753823397d2b1d1c3fb
freittin <nideologies@gmail.com>
2022-07-08 12:40:32 -0700

    hot fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,15 +7,14 @@
 
 void print_most_numbers(void)
 {
-	int number;
-
-	for (number = 48; number < 58; number++)
-	{
-		if ((number == 50 || number == 52))
-		{
-			continue;
-		}
-			_putchar(number);
-	}
-	putchar ('\n);
+int number;
+for (number = 48; number < 58; number++)
+{
+if ((number == 50) || (number == 52))
+{
+continue;
+}
+_putchar(number);
+}
+_putchar ('\n);
 }
