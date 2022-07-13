5ec17f549d5cfd16a9c5462af7a4babea4f84853
freittin <nideologies@gmail.com>
2022-07-13 12:53:14 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy -> function that copies a string
+ * @dest: parameter one
+ * @src: parameter two
+ * @n: parameter three
+ * Return: string as success
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
