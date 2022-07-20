6cc3365c48e9638cf1776f1f84abb1af3fecf4dc
freittin <nideologies@gmail.com>
2022-07-20 12:29:06 -0700

    reverse string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,12 +3,12 @@
 /**
  * _print_rev_recursion - prints a string followed by a new line
  * @s: source
- * Return: string
+ * Return: void
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s == '\0') /* base case */
 	{
 		_putchar('\n');
 		return;
