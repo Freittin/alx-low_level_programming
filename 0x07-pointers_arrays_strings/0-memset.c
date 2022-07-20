f7456ab93eae1abbce40e90415526e72f3747ce8
freittin <nideologies@gmail.com>
2022-07-19 19:55:58 -0700

    quick edits

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
+ * _memset -> fills memory with a constant byte
  * @s: source
  * @b: contant byte
  * @n: buffer length
- * Return : new string
+ * Return: new string
  */
 
 char *_memset(char *s, char b, unsigned int n)
