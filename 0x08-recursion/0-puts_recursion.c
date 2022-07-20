38e5a5900506224756d48e369c57761fbbb68c2e
freittin <nideologies@gmail.com>
2022-07-20 12:32:54 -0700

    reverse string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string followed by a new line
+ * _puts_recursion - prints a string followed by a new line
  * @s: source
  * Return: void
  */
 
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s == '\0') /* base case */
 	{
