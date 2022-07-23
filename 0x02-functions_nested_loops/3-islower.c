b857ed34487b888ced555c2c94ed7576dc3d3c02
freittin <nideologies@gmail.com>
2022-07-23 10:14:28 -0700

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,17 +3,11 @@
 
 /**
  * _islower -> checks for lowercase character
+ * @c: an argument
  * Return: returns 1 and 0 depending on condition
  */
 
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		returns (1);
-	}
-	else
-	{
-		returns (0);
-	}
+	return (c >= 'a' && c <= 'z');
 }
