ada11b3f109ec9569043bd1b155ca85a2cad3089
freittin <nideologies@gmail.com>
2022-07-26 08:58:20 -0700

    one

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicates string given as a param
+ * @str: string to be copied
+ * Return: duplicated string
+ */
+
+char *_strdup(char *str)
+{
+int i;
+char *s;
+
+if (str == NULL)
+return (NULL);
+while (str[i])
+i++;
+s = (char *)malloc(i * sizeof(char) + 1);
+if (s == NULL)
+return (NULL);
+while (j < i)
+{
+s[j] = str[j];
+j++;
+}
+s[j] = '\0';
+return (s);
+}
