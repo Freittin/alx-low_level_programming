70c0dc49e7397e65d8f3e399d506c5b87870ce52
freittin <nideologies@gmail.com>
2022-08-01 18:58:02 -0700

    first serious header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,24 @@
 #ifndef STRUCT_DOG
 #define STRUCT_DOG
 
-#define struct dog
+/**
+ * struct dog - structure of doge
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: my name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog Dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
